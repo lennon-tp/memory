97969a68f8ee4ed4c554622eea5cedb4bf19ca34
lennon-tp <lennontchenpan@gmail.com>
2026-04-06 12:04:57 -1000

    feat: configuration de capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'memory',
-  webDir: 'www'
+  appId: 'com.lennon-tp.memory',  
+  appName: 'Memory',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
