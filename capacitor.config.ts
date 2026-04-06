437f4b4fb5c93e56628eba7b4f12d0d19a67a0c6
lennon-tp <lennontchenpan@gmail.com>
2026-04-06 12:46:12 -1000

    fix: correction de l'appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.lennon-tp.memory',  
+  appId: 'com.lennontp.memory',  
   appName: 'Memory',
   webDir: 'www',
   server: {
